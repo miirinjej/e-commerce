c0ffcf1e552a51fa7d058dcd514dec85099b45b3
miirinjej <miirinjej@gmail.com>
2019-03-29 05:07:53 +0500

    Create new project via nuxt template

---
+++ README.md
@@ -0,0 +1,28 @@
+# e-commerce
+
+A web app developed from [shop](https://www.behance.net/gallery/26635211/Shop-Template-free-PSD) free template 
+designed by [Andrey Shepkin](https://www.behance.net/hellobarty) art director © 2015, available via 
+license: [CC BY-NC 4.0](https://creativecommons.org/licenses/by-nc/4.0/deed.en_US)
+
+## Preview
+
+[View live preview](https://miirinjej.github.io/e-commerce/)
+
+## Build Setup
+
+``` bash
+# install dependencies
+$ npm install
+
+# serve with hot reload at localhost:3000
+$ npm run dev
+
+# build for production and launch server
+$ npm run build
+$ npm start
+
+# generate static project
+$ npm run generate
+```
+
+For detailed explanation on how things work, checkout [Nuxt.js docs](https://nuxtjs.org).
---
+++ nuxt.config.js
@@ -0,0 +1,60 @@
+import pkg from './package';
+
+export default {
+  mode: 'universal',
+
+  /*
+  ** Headers of the page
+  */
+  head: {
+    htmlAttrs: {
+      lang: 'en-US',
+    },
+    title: pkg.name,
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: pkg.description },
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+    ],
+  },
+
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#ffffff' },
+
+  /*
+  ** Global CSS
+  */
+  css: [
+  ],
+
+  /*
+  ** Plugins to load before mounting the App
+  */
+  plugins: [
+  ],
+
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+    // Doc: https://bootstrap-vue.js.org/docs/
+    'bootstrap-vue/nuxt',
+    '@nuxtjs/pwa',
+  ],
+
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** You can extend webpack config here
+    */
+    extend(config, ctx) {
+    },
+  },
+};
